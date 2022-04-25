c879d0a34b5ef0521e68e764d5f64ea754259bf6
Ibrahim <smarsatto@gmail.com>
2022-04-25 23:02:38 +0100

    detailing was neccesary

--- src/app/country-details/country-details.component.ts
+++ src/app/country-details/country-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Holidays } from '../core/models/country';
+import { Country, Holidays } from '../core/models/country';
 import { Loader } from '../core/models/loader';
 import { AppState } from '../core/models/state';
 import { ApiService } from '../core/services/api/api.service';
@@ -17,6 +17,7 @@ export class CountryDetailsComponent implements OnInit {
   loading = false;
   holidays: Holidays[] = [];
   loaderType: Loader = { type: 'list' };
+  country: any;
   constructor(
     private activateDroute: ActivatedRoute,
     private countryS: ApiService,
@@ -26,6 +27,11 @@ export class CountryDetailsComponent implements OnInit {
 
   ngOnInit() {
     const countrySlug = this.activateDroute.snapshot.paramMap.get('slug')!;
+
+    this.store.select((store) => store.countryList).pipe(take(1)).subscribe((countries) => {
+      this.country = countries.find((country) => country.code === countrySlug);
+    });
+
   this.store.select((store) => store.countryHolidays).pipe(take(1)).subscribe((holidays) => {
     if (holidays.length > 0 && holidays.filter((holiday) => holiday.country_code === countrySlug).length > 0) {
       this.holidays = holidays.filter((holiday) => holiday.country_code.toLowerCase() === countrySlug.toLowerCase());
